d6e839e4d1c6b2b60ffacd7ca06afc8066eb9a5b
Mark Harfouche <mark.harfouche@gmail.com>
2025-09-07 16:45:15 -0400

    Help find runtimes within conda-forge

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,13 @@ set -ex
 mkdir build
 pushd build
 
-cmake ${CMAKE_ARGS} ..
+# ENABLE_LIBDIR_IN_RUNTIME_SEARCH=ON
+# help VPL find runtimes within the conda-forge environment.
+# https://github.com/intel/libvpl/issues/119
+cmake \
+    ${CMAKE_ARGS} \
+    -DENABLE_LIBDIR_IN_RUNTIME_SEARCH=ON \
+    ..
 
 make -j${CPU_COUNT}
 
